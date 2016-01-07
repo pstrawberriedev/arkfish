8b9b8e2b654677d0885db3d572bda46a2083568d
Josh DePaolo <pstrawberriedev@gmail.com>
2016-01-06 18:41:08 -0700

    got it

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,45 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+var steamServerStatus = require('steam-server-status');
 
+var unicornInfo = {};
+
+//Functions for Servers
+function getStatusUnicorn() {
+  steamServerStatus.getServerStatus(
+    'arkfish.net', 27015, function(serverInfo) {
+      if (serverInfo.error) {
+        console.log(serverInfo.error);
+      } else {
+        console.log(serverInfo);
+        unicornInfo = serverInfo;
+        //console.log("game: " + serverInfo.gameName);
+        //console.log("server name: " + serverInfo.serverName);
+        //console.log("players: " + serverInfo.numberOfPlayers + "/" + serverInfo.maxNumberOfPlayers)
+      }
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var arkInfo = {};
-	request('https://api.ark.bar/v1/server/arkfish.net/27015', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var arkInfo = JSON.parse(body);
-			console.log('all\n' + body);
-			console.log('Status: ' + arkInfo.status);
-		}
-	})
-  res.render('home', { title: 'Arkfish', info: arkInfo });
+  
+  getStatusUnicorn();
+
+  res.render('home', { 
+    title: 'Status', 
+    unicornInfo: unicornInfo
+  });
 });
 
 module.exports = router;
+
+/*
+var arkInfo = {};
+request('https://api.ark.bar/v1/server/arkfish.net/27015', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var arkInfo = JSON.parse(body);
+    console.log('all\n' + body);
+    console.log('Status: ' + arkInfo.status);
+  }
+})*/
