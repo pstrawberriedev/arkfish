3e5fe710ecbf614cf2e88c2a8cb08067784a570d
JD <pstrawberrie@gmail.com>
2016-06-01 19:58:27 -0600

    js typo

--- public/scripts/arkfish.js
+++ public/scripts/arkfish.js
@@ -7,7 +7,7 @@ console.log('--> arkfish.js');
 
 $(document).ready(function() {
   
-  var newStr = $('#home h1').text().replace('-', '\n').replace('(', '').replace('(', '');
+  var newStr = $('#home h1').text().replace('-', '\n').replace('(', '').replace(')', '');
 $('#home h1').text(newStr);
   
 });
