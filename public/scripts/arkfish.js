b446843dc72a003e212d638835e17f7555a75b3a
JD <pstrawberrie@gmail.com>
2016-06-01 19:56:19 -0600

    formatting server name with js

--- public/scripts/arkfish.js
+++ public/scripts/arkfish.js
@@ -5,3 +5,9 @@
 //Hello
 console.log('--> arkfish.js');
 
+$(document).ready(function() {
+  
+  var newStr = $('#home h1').text().replace('-', '\n').replace('(', '').replace('(', '');
+$('#home h1').text(newStr);
+  
+});
